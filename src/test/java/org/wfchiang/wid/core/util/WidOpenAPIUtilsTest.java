f9fee9d42ed771979434e77d6ef4b5822d262d41
wfchiang <weifan.wf@gmail.com>
2020-05-16 21:39:03 -0400

    Add the unit tests for DefaultObjectEnumeratorTest

--- src/test/java/org/wfchiang/wid/core/util/WidOpenAPIUtilsTest.java
+++ src/test/java/org/wfchiang/wid/core/util/WidOpenAPIUtilsTest.java
@@ -8,66 +8,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.Assert;
+import org.wfchiang.wid.core.TestingUtils;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class WidOpenAPIUtilsTest {
 
-    private OpenAPI getOpenAPIFromClassPath (String resourceClassPath) throws IOException {
-        Assert.assertNotNull(resourceClassPath);
-
-        String content = WidIOUtils.readClassPathResourceContent(resourceClassPath);
-        OpenAPI openAPI = WidOpenAPIUtils.getOpenAPIFromContent(content);
-        Assert.assertNotNull(openAPI);
-        return openAPI;
-    }
-
-    private Map<String, Schema> getComponentSchemasFromOpenAPI (OpenAPI openAPI) {
-        Assert.assertNotNull(openAPI);
-        Assert.assertNotNull(openAPI.getComponents());
-        Assert.assertNotNull(openAPI.getComponents().getSchemas());
-        return openAPI.getComponents().getSchemas();
-    }
-
-    private ObjectSchema getObjectSchemaFromDefinitions (String schemaName, Map<String, Schema> definitions) {
-        Assert.assertNotNull(schemaName);
-        Assert.assertNotNull(definitions);
-        Assert.assertTrue(definitions.containsKey(schemaName));
-
-        Schema schema = definitions.get(schemaName);
-        Assert.assertNotNull(schema);
-        Assert.assertTrue(schema instanceof ObjectSchema);
-        return (ObjectSchema) schema;
-    }
-
-    private StringSchema getStringSchemaFromDefinitions (String schemaName, Map<String, Schema> definitions) {
-        Assert.assertNotNull(schemaName);
-        Assert.assertNotNull(definitions);
-        Assert.assertTrue(definitions.containsKey(schemaName));
-
-        Schema schema = definitions.get(schemaName);
-        Assert.assertNotNull(schema);
-        Assert.assertTrue(schema instanceof StringSchema);
-        return (StringSchema) schema;
-    }
-
     @Test
     public void getOpenAPIFromContent_0 () throws IOException {
-        OpenAPI openAPI = this.getOpenAPIFromClassPath("examples/ex0.yml");
+        OpenAPI openAPI = TestingUtils.getOpenAPIFromClassPath("examples/ex0.yml");
 
-        Map<String, Schema> componentSchemas = this.getComponentSchemasFromOpenAPI(openAPI);
+        Map<String, Schema> componentSchemas = TestingUtils.getComponentSchemasFromOpenAPI(openAPI);
 
-        ObjectSchema objectSchema0 = this.getObjectSchemaFromDefinitions("Object0", componentSchemas);
+        ObjectSchema objectSchema0 = TestingUtils.getObjectSchemaFromDefinitions("Object0", componentSchemas);
 
         Map<String, Schema> schema0Properties = objectSchema0.getProperties();
 
-        StringSchema stringSchema0 = this.getStringSchemaFromDefinitions("strKey0", schema0Properties);
+        StringSchema stringSchema0 = TestingUtils.getStringSchemaFromDefinitions("strKey0", schema0Properties);
         Assert.assertNotNull(stringSchema0.getMaxLength());
         Assert.assertEquals(3, stringSchema0.getMaxLength().intValue());
         Assert.assertNull(stringSchema0.getExample());
 
-        StringSchema stringSchema1 = this.getStringSchemaFromDefinitions("strKey1", schema0Properties);
+        StringSchema stringSchema1 = TestingUtils.getStringSchemaFromDefinitions("strKey1", schema0Properties);
         Assert.assertNull(stringSchema1.getMaxLength());
         Assert.assertNotNull(stringSchema1.getExample());
     }
